b1c39ca9623cd00360128081e1b4c3b36b46e4da
Hariharan <hariharan@apples-MacBook-Pro-3.local>
2021-10-10 00:01:00 -0400

    Graph Data 2021-10-10T00:01

--- manipulate.py
+++ manipulate.py
@@ -3,6 +3,6 @@ for line_num, line in enumerate(open('script.sh').readlines()):
     if line_num % 2 == 1:
         lines.append(line)
     else:
-        lines.append('echo $RANDOM\n')
+        lines.append('echo $RANDOM > file.txt \n')
 
 open('script.sh', 'w').writelines(lines)
